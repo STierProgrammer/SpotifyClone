3cad883941467a65d0d21972f2c64d823f3e16a4
STierProgrammer <sabasajaia40@gmail.com>
2024-11-30 22:43:18 +0400

    Init

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const primary = Color(0xff42C83C);
+  static const lightBackground = Color(0xfff6f6f6);
+  static const darkBackground = Color(0xff2a2a2a);
+  static const grey = Color(0xffa2a2a2);
+  static const darkGrey = Color(0xff484848);
+}
